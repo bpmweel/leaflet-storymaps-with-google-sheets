17f29f823aaa6c9b14452d0946375392593606ee
Berend Weel <b.weel@esciencecenter.nl>
2020-03-31 17:07:51 +0200

    Use correct url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSmI730OkM-bmov2FQ67hDEqMXO24SBIyf57et0jm0b0NlWhnZIoOabBKhbjfBeaM0w8FydR_cTgqpp/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZKQDvLsKaoj1fqg6l9AFOGzcu-3HYTymk3281oRHrf4/';
