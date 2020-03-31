efb5b1daff10b57ff9b723e5461dca366191f570
Berend Weel <b.weel@esciencecenter.nl>
2020-03-31 17:03:58 +0200

    Updated links

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSmI730OkM-bmov2FQ67hDEqMXO24SBIyf57et0jm0b0NlWhnZIoOabBKhbjfBeaM0w8FydR_cTgqpp/pubhtml';
